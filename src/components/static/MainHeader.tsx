00701fec0b3277a14845db06cefb01543e83291f
blessingchidima <chukwuyenumblessing8@gmail.com>
2023-08-24 12:54:26 +0100

    fiv

--- src/components/static/MainHeader.tsx
+++ src/components/static/MainHeader.tsx
@@ -1,76 +1,102 @@
-import { useState } from "react"
-import {GiLaurelCrown} from "react-icons/gi"
+import { useState } from "react";
+import { GiLaurelCrown } from "react-icons/gi";
 // import { Link } from "react-router-dom"
-import {MdOutlineArrowDropDown} from "react-icons/md"
-
-
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const MainHeader = () => {
-
-  const [scroll, setScroll] = useState<boolean>(false)
+  const [scroll, setScroll] = useState<boolean>(false);
   const onScroll = () => {
-    setScroll(scroll)
+    setScroll(scroll);
 
-    if(window.scrollY >= 40 ){
-      setScroll(true)
-    }else{
-      setScroll(false)
+    if (window.scrollY >= 40) {
+      setScroll(true);
+    } else {
+      setScroll(false);
     }
-  }
-  window.addEventListener("scroll", onScroll)
+  };
+  window.addEventListener("scroll", onScroll);
 
-const [hover, setHover ] = useState<boolean>(false)
-const onHover = () => {
-  setHover(!hover)
-}
+  const [hover, setHover] = useState<boolean>(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
 
   return (
     <div>
-       {
-        scroll ?  <div className="w-full h-[60px] flex items-center justify-center bg-black text-white fixed duration-700 z-10 ">
-        <div className="w-[93%] h-[50px] flex justify-between items-center ">
+      {scroll ? (
+        <div className="w-full h-[60px] flex items-center justify-center bg-black text-white fixed duration-700 z-10 ">
+          <div className="w-[93%] h-[50px] flex justify-between items-center ">
             <div className="flex items-center justify-center">
-                <div className="w-[35px] h-[35px] rounded-[50%] overflow-hidden flex items-center justify-center"><GiLaurelCrown className ="w-[35px] h-[35px] "/></div>
-                <div className="ml-[7px] font-Poppins mt-[3px]">Monao</div>
+              <div className="w-[35px] h-[35px] rounded-[50%] overflow-hidden flex items-center justify-center">
+                <GiLaurelCrown className="w-[35px] h-[35px] " />
+              </div>
+              <div className="ml-[7px] font-Poppins mt-[3px]">Monao</div>
             </div>
+            <div className="w-[80%] h-[40px] flex items-center justify-end">
+              <div>Create task </div>
+           
+            </div>
+            <div
+              className="flex items-center cursor-pointer"
+              onMouseEnter={onHover}
+              onMouseLeave={onHover}
+            >
+              {hover ? (
+                <div className="absolute bg-gray-700 text-white px-8 py-2 rounded text-[14px] font-semibold mt-[60px] ml-[15px] cursor-pointer ">
+                  Logout
+                </div>
+              ) : null}
+              <div className="w-[40px] h-[40px] rounded-[50%] bg-white mr-[5px] flex items-center justify-center">
+                Img
+              </div>
+              <div className="font-semibold text-[13px] ml-[px] ">Username</div>
+              <div>
+                <MdOutlineArrowDropDown />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full h-[60px] flex items-center justify-center  fixed duration-700 z-10 ">
+          <div className="w-[93%] h-[50px] flex justify-between items-center ">
+            <div className="flex items-center justify-center">
+              <div className="w-[35px] h-[35px] rounded-[50%] overflow-hidden flex items-center justify-center">
+                <GiLaurelCrown className="w-[35px] h-[35px] " />
+              </div>
+              <div className="ml-[7px] font-Poppins mt-[3px]">Monao</div>
+            </div>
+            {/* <div>Create task</div> */}
             {/* <div className="cursor-pointe">About Us</div> */}
             {/* <div>
                 <Link to="/signin" className="px-8 py-2 rounded-[30px] border border-[grey]  font-Poppins">Logout</Link>
             </div> */}
-            <div className="flex items-center cursor-pointer" onMouseEnter={onHover} onMouseLeave={onHover}>
-              {
-                hover? <div className="absolute bg-gray-700 text-white px-8 py-2 rounded text-[14px] font-semibold mt-[60px] ml-[15px] cursor-pointer ">Logout</div>: null
-              }
-              <div className="w-[40px] h-[40px] rounded-[50%] bg-white mr-[5px] flex items-center justify-center">Img</div>
+            <div className="w-[80%] h-[40px] flex items-center justify-end">
+              <div>Create task</div>
+            </div>
+            <div
+              className="flex items-center cursor-pointer"
+              onMouseEnter={onHover}
+              onMouseLeave={onHover}
+            >
+              {hover ? (
+                <div className="absolute bg-black text-white px-8 py-2 rounded text-[14px] font-semibold mt-[60px] ml-[15px] cursor-pointer ">
+                  Logout
+                </div>
+              ) : null}
+              <div className="w-[40px] h-[40px] rounded-[50%] bg-black mr-[5px] flex items-center justify-center">
+                Img
+              </div>
               <div className="font-semibold text-[13px] ml-[px] ">Username</div>
-              <div><MdOutlineArrowDropDown/></div>
-            
+              <div>
+                <MdOutlineArrowDropDown />
+              </div>
             </div>
+          </div>
         </div>
-    </div> :  <div className="w-full h-[60px] flex items-center justify-center  fixed duration-700 z-10 ">
-        <div className="w-[93%] h-[50px] flex justify-between items-center ">
-            <div className="flex items-center justify-center">
-                <div className="w-[35px] h-[35px] rounded-[50%] overflow-hidden flex items-center justify-center"><GiLaurelCrown className ="w-[35px] h-[35px] "/></div>
-                <div className="ml-[7px] font-Poppins mt-[3px]">Monao</div>
-            </div>
-            {/* <div className="cursor-pointe">About Us</div> */}
-            {/* <div>
-                <Link to="/signin" className="px-8 py-2 rounded-[30px] border border-[grey]  font-Poppins">Logout</Link>
-            </div> */}
-            <div className="flex items-center cursor-pointer" onMouseEnter={onHover} onMouseLeave={onHover}>
-              {
-                hover? <div className="absolute bg-black text-white px-8 py-2 rounded text-[14px] font-semibold mt-[60px] ml-[15px] cursor-pointer ">Logout</div>: null
-              }
-              <div className="w-[40px] h-[40px] rounded-[50%] bg-black mr-[5px] flex items-center justify-center">Img</div>
-              <div className="font-semibold text-[13px] ml-[px] ">Username</div>
-              <div><MdOutlineArrowDropDown/></div>
-            
-            </div>
-        </div>
+      )}
     </div>
-       }
-    </div>
-  )
-}
+  );
+};
 
-export default MainHeader
+export default MainHeader;
--- src/pages/screen/HomeScreen.tsx
+++ src/pages/screen/HomeScreen.tsx
@@ -1,12 +1,27 @@
-// import { IoIosArrowDropdownCircle } from "react-icons/io";
+import img from "../../assets/look.webp"
+import img1 from "../../assets/convo.webp"
+import img2 from "../../assets/map.webp"
 const HomeScreen = () => {
   return (
-    <div className="w-full h-[auto] flex justify-center">
-      <div className="mt-[60px] w-[93%] h-auto">
-        <div className="bg-slate-500 h-full w-full flex  justify-center items-center">
-          Start Here
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex h-[100px]">
+        <div className="w-[600px] h-[450px] mr-5 bg-red-500  flex justify-center items-center rounded-md">
+          <div className="w-[200px] h-[150px] bg-[white] m-3">
+           <img src={img}  className="object-cover h-full w-full"/>
+          </div>
+          <div className="w-[180px] h-[150px] bg-[blue] m-3">
+            <img src={img1}/>
+          </div>
+          <div className="w-[180px] h-[150px] bg-[white] m-3">
+            <img src={img2}/>
+          </div>
+
+          box
         </div>
-      </div>{" "}
+        <div className="w-[600px] h-[450px] bg-blue-500 flex justify-center items-center rounded-md">
+          box
+        </div>
+      </div>
     </div>
   );
 };
